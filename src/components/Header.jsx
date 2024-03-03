e170fe89828f7c65d5ce0a3d8b64e8c7d38a72df
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-04 02:40:35 +0530

    use of useProgresscontext to show the context of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
 import CartContext from '../store/CardContext';
+import UserProgressContext from '../store/UserProgressContext';
+
 export default function Header() {
+  const userProgressCtx = useContext(UserProgressContext);
   const cartCtx = useContext(CartContext);
   const totalCartItems = cartCtx.items.reduce((totalNoOfItems, item) => {
     return totalNoOfItems + item.quantity;
-  },0);
+  }, 0);
 
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -15,7 +21,9 @@ export default function Header() {
         <h1>ReactFoods</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
