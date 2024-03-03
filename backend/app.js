da2c0fc11924e7198e699ce6d11a0b9f7351dec2
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-03 18:39:45 +0530

    add the return keyword to the meals api

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.get('/meals', async (req, res) => {
   const meals = await fs.readFile('./data/available-meals.json', 'utf8');
-  res.json(JSON.parse(meals));
+  return res.json(JSON.parse(meals));
 });
 
 app.post('/orders', async (req, res) => {
