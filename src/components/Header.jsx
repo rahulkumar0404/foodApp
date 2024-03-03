44e5cbb0099422eb00be5da200c4ae7d3633c9a7
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-03 23:02:28 +0530

    used the cardContext to the component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
-import logoImg from '../assets/logo.jpg'
-import Button from './UI/Button'
+import { useContext } from 'react';
+import logoImg from '../assets/logo.jpg';
+import Button from './UI/Button';
+import CartContext from '../store/CardContext';
 export default function Header() {
-    return (
-      <header id="main-header">
-        <div id='title'>
-          <img src={logoImg} alt="Logo-Image" />
-          <h1>ReactFoods</h1>
-        </div>
-        <nav>
-          <Button textOnly >Cart (0)</Button>
-        </nav>
-      </header>
-    );
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((totalNoOfItems, item) => {
+    return totalNoOfItems + item.quantity;
+  },0);
+
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="Logo-Image" />
+        <h1>ReactFoods</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
+    </header>
+  );
 }
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
 import { currencyFormatter } from '../utils/formatting';
 import Button from './UI/Button';
+import CardContext from '../store/CardContext';
 export default function MealItem({ meal }) {
+  const cartctx = useContext(CardContext);
+  function handleAddMealToCart() {
+    cartctx.addItem(meal);
+  }
   return (
     <li className="meal-item">
       <article>
@@ -13,7 +19,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
