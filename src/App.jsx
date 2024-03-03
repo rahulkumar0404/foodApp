d99ce502c0e6534d886bc880dcc26f8685f6ee4d
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-04 02:41:27 +0530

    added the context as a wrapper to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Header from './components/Header';
 import Meals from './components/Meals';
+import Cart from './components/Cart'
 import { CardContextProvider } from './store/CardContext';
+import { UserProgressContextProvider } from './store/UserProgressContext';
 function App() {
   return (
-    <CardContextProvider>
-      <Header />
-      <Meals />
-    </CardContextProvider>
+    <UserProgressContextProvider>
+      <CardContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CardContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
