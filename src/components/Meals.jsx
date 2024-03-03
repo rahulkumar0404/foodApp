efd01c25b9636a9f8d94f5fd2d4d95258d5f5c09
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-03 18:41:40 +0530

    added the mealItem component to the mealscomponent

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-
+import MealItem from './MealItem'
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
 
   useEffect(() => {
     async function fetchMeals() {
-      const response = fetch('http:localhost:3000/meals');
+      const response = await fetch('http://localhost:3000/meals');
       if (!response.ok) {
         throw new Error("Failed to load the value");
       }
@@ -16,9 +16,9 @@ export default function Meals() {
   }, []);
   return (
     <ul id="meals">
-      {loadedMeals.map((meal) => {
-        <li key={meal.id}>{meal.name}</li>;
-      })}
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal}/>
+      ))}
     </ul>
   );
 }
