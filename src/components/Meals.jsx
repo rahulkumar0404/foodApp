c2016024482a5f90012255c6e100f4738d0c1948
Rahul@kumar <drkagarwal05@gmail.com>
2024-03-04 22:30:35 +0530

    used the custom hook to load data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,36 @@
-import { useState, useEffect } from 'react';
-import MealItem from './MealItem'
-export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+import MealItem from './MealItem';
+import useHttp from '../hooks/useHttp';
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
-      if (!response.ok) {
-        throw new Error("Failed to load the value");
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals()
-  }, []);
+const requestConfig = {};
+export default function Meals() {
+  // const [loadedMeals, setLoadedMeals] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     const response = await fetch('');
+  //     if (!response.ok) {
+  //       throw new Error("Failed to load the value");
+  //     }
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
+  //   fetchMeals()
+  // }, []);
+
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  console.log(loadedMeals);
+  if (isLoading) {
+    return <p>Fetching meal...</p>;
+  }
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <MealItem key={meal.id} meal={meal}/>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
